a13dad5f8da94872b52a879676eb4352d2e2745e
Dennis Dang <dangggdennis@gmail.com>
2019-04-23 23:28:38 -0700

    ast and parser start

---
+++ ast/ast.go
@@ -0,0 +1,58 @@
+package ast
+
+import "monkey/token"
+
+// Node is a single unit in our AST
+type Node interface {
+	TokenLiteral() string
+}
+
+// Statement is the combo of Identifier and Expression
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// Expression is the evaluated value of a Statement
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Program is the root node of every AST our parser produces
+type Program struct {
+	Statements []Statement
+}
+
+// TokenLiteral will be used only for debugging and testing
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+
+	return ""
+}
+
+// LetStatement is a Monkey let statement
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+
+// TokenLiteral of a LetStatement is "let"
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+// Identifier is the left hand side of a LetStatement
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+
+// TokenLiteral of an Identifier is the user's variable name
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
---
+++ parser/parser.go
@@ -0,0 +1,36 @@
+package parser
+
+import (
+	"monkey/ast"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+// Parser is our Monkey parser!
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token // Look to current token under examination, to decide what to do next
+	peekToken token.Token // In case curToken does not given enough information
+}
+
+// New instantiates a Parser object with the result of a lexer
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Read two tokens, so curTokens and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+// ParseProgram begins the parsing process
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
