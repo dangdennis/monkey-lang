bf125a83c53b2e4e67e1df7835fbb4fd6e421d32
Dennis Dang <dangggdennis@gmail.com>
2024-09-20 15:24:19 -0600

    setup utest and scala cli

--- scala/src/Main.test.scala
+++ scala/src/Main.test.scala
@@ -4,17 +4,17 @@ import utest._
 
 object MainTests extends TestSuite {
   val tests = Tests {
-    val x = 1
+    val x = 2
     test("outer1") {
       val y = x + 1
 
       test("inner1") {
-        assert(x == 1, y == 2)
+        assert(x == 2, y == 3)
         (x, y)
       }
       test("inner2") {
         val z = y + 1
-        assert(z == 3)
+        assert(z == 4)
       }
     }
     test("outer2") {
